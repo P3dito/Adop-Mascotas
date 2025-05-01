98ea3e1a88f316086ba1a7677245b0b59e18eefc
P3dito <palominozaratep@gmail.com>
2025-05-01 14:19:30 -0500

    Creación de DockerFile

--- obj/Debug/net8.0/Adop_mascotas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Adop_mascotas.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41bf3a22ff78b51fcf8436b6787209e96c0f543f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd31d9135a7be1c1289fc9f26669ee3b63b4776d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

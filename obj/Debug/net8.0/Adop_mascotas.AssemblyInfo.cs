9d03b0d94d1c155820933984a4b95457852cdc22
P3dito <palominozaratep@gmail.com>
2025-05-01 14:25:47 -0500

    Correción 3

--- obj/Debug/net8.0/Adop_mascotas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Adop_mascotas.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e19929ae811163075b3c4adb88cd1b3f5ee2cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827035d9732022a096d54a05ad109cc53b8fb6aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adop_mascotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

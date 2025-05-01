c59b92b4c8aff7fc677fcf65a9655d4a8fea57db
P3dito <palominozaratep@gmail.com>
2025-05-01 13:16:40 -0500

    Inicializando Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Adop_mascotas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
